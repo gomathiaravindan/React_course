7698b5b5f0024497b9bc5f061c879ca24e2b720b
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2020-10-19 18:49:45 +0530

    App component files are added.

---
+++ React_app_components/intro.js
@@ -0,0 +1,11 @@
+class About extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h2>This course offer the basics React and NextJs!!</h2>
+                <h2>Become a powerful front-end developer!!</h2>
+            </div>
+        )
+    }
+}
---
+++ React_app_components/welcome.js
@@ -0,0 +1,11 @@
+class Intro extends React.Component{
+    render()
+    {
+        return (
+            <div>
+                <h3>Welcome all!!</h3>
+                <h3>Happy to see you all in this wonderful course!!</h3>
+            </div>
+        );
+    }
+}
